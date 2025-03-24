67cfa1e4aa2db87130fbd77fa124e97a6ddde12e
artem <ArtemKorolev24092009@yandex.ru>
2025-03-24 23:33:37 +0300

    io factory now returns cli object

--- Include/IOInterfaceFactory.hpp
+++ Include/IOInterfaceFactory.hpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include "IOInterface.hpp"
+#include "ConsoleInterface.hpp"
 
 
 class IOInterfaceFactory {
--- Source/IOInterfaceFactory.cpp
+++ Source/IOInterfaceFactory.cpp
@@ -1,18 +1,6 @@
 #include "IOInterfaceFactory.hpp"
 
 
-class IO_Mock : public IOInterface
-{
-public:
-    IO_Mock(FieldInteractor &field)
-        : IOInterface(field)
-    {}
-
-    void gameLoop() override {
-        // pass
-    }
-};
-
 std::unique_ptr<IOInterface> IOInterfaceFactory::getIOInterface(FieldInteractor &field) {
-    return std::make_unique<IO_Mock>(field);
+    return std::make_unique<ConsoleInterface>(field);
 }
