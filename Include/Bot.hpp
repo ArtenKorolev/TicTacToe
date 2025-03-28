7f470b1c86b023b369772257bc6b9092a97c6499
artem <ArtemKorolev24092009@yandex.ru>
2025-03-28 12:07:06 +0300

    add bot class separately from core

---
+++ Include/Bot.hpp
@@ -0,0 +1,14 @@
+#ifndef BOT_HPP
+#define BOT_HPP
+
+#include "Field.hpp"
+#include "Utils.hpp"
+
+
+class Bot {
+public:
+    void makeStep(FieldInteractor &field);
+};
+
+
+#endif // BOT_HPP
--- Include/Core.hpp
+++ Include/Core.hpp
@@ -4,17 +4,19 @@
 #include <stdexcept>
 #include "Utils.hpp"
 #include "Field.hpp"
+#include "Bot.hpp"
 
 
 class Core {
 public:
-    Core(FieldInteractor &field);
+    Core(FieldInteractor &field, Bot &bot);
     void userStep(Coordinates coordinates);
     void computerStep();
     bool checkWinPosition();
     bool checkDrawPosition();
 private:
     FieldInteractor &_field;
+    Bot &_bot;
 };
 
 
