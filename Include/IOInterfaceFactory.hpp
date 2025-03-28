4f51e2f5f706eb88c5d90bf8d1e575cd270f69d3
artem <ArtemKorolev24092009@yandex.ru>
2025-03-28 11:57:46 +0300

    leave only mock interface

--- Include/IOInterfaceFactory.hpp
+++ Include/IOInterfaceFactory.hpp
@@ -3,7 +3,6 @@
 
 #include <memory>
 #include "IOInterface.hpp"
-#include "ConsoleInterface.hpp"
 
 
 class IOInterfaceFactory {
--- Source/IOInterfaceFactory.cpp
+++ Source/IOInterfaceFactory.cpp
@@ -1,6 +1,16 @@
 #include "IOInterfaceFactory.hpp"
 
 
+class IO_Mock : public IOInterface {    
+public:
+    IO_Mock(FieldInteractor &field) : IOInterface(field) {}
+
+    void gameLoop() {
+        // pass
+    }
+};
+
+
 std::unique_ptr<IOInterface> IOInterfaceFactory::getIOInterface(FieldInteractor &field) {
-    return std::make_unique<ConsoleInterface>(field);
+    return std::make_unique<IO_Mock>(field);
 }
