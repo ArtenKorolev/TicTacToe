5cf18140441dab3339a17af6657b552264b0128c
artem <ArtemKorolev24092009@yandex.ru>
2025-03-29 21:30:10 +0300

    fix cmakelists.txt and refactor sfml interface

--- Include/SFMLInterface.hpp
+++ Include/SFMLInterface.hpp
@@ -16,17 +16,20 @@ public:
     ~SFMLInterface() override = default;
     void gameLoop() override;
 private:
-    void _handleEvents(sf::RenderWindow &window);
+    void _handleEvent(sf::Event &event, sf::Window &window);
+    void _handleUserStep(sf::Event &event);
+    void _handleComputerStep();
+    bool _tryToMakeUserStep(sf::Event &event);
+    Coordinates _getCoordinatesFromEvent(sf::Event &event);
+    bool _checkForWinOrDraw(const std::string &player);
     void _drawField(sf::RenderWindow &window);
     void _drawCell(sf::RenderWindow &window, int x, int y);
+    void _drawEmptyCell(sf::RenderWindow &window, int x, int y);
+    void _drawCross(sf::RenderWindow &window, int x, int y);
+    void _drawZero(sf::RenderWindow &window, int x, int y);
     Core _core;
-    bool _playerCanMove = true;
+    bool _hasWinner = false;
     Bot _bot;
-};
-
-
-class SFMLUtils {
-public:
 };
 
 
--- Source/SFMLInterface.cpp
+++ Source/SFMLInterface.cpp
@@ -6,57 +6,76 @@ SFMLInterface::SFMLInterface(FieldInteractor &field)
 {}
 
 void SFMLInterface::gameLoop() {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "TicTacToe");
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_WIDTH), "Tic Tac Toe Game!");
+    window.setFramerateLimit(60); 
 
-    while (window.isOpen()) {
-        sf::Event event;
-        _handleEvents(window);
-        window.clear(sf::Color::Black);
-        _drawField(window);
-        window.display();
-    }
-
-}
-
-// TODO: Refactor this function
-
-void SFMLInterface::_handleEvents(sf::RenderWindow &window) {
     sf::Event event;
 
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            window.close();
-        } 
-        else if (event.type == sf::Event::MouseButtonPressed && _playerCanMove) {
-            if (event.mouseButton.button == sf::Mouse::Left) {
-                int x = event.mouseButton.x / CELL_SIZE;
-                int y = event.mouseButton.y / CELL_SIZE;
-                Coordinates coordinates{x, y};
-                try {
-                    _core.userStep(coordinates);
-                    if (_core.checkWinPosition()) {
-                        GAME_FILE_LOGGER.log("Пользователь выиграл!");
-                        _playerCanMove = false;
-                        return;
-                    } else if (_core.checkDrawPosition()) {
-                        _playerCanMove = false;
-                        GAME_FILE_LOGGER.log("Ничья!");
-                        return;
-                    }
-                    _core.computerStep();
-                    if (_core.checkWinPosition()) {
-                        _playerCanMove = false;
-                        GAME_FILE_LOGGER.log("Компьютер выиграл!");
-                    } else if (_core.checkDrawPosition()) {
-                        _playerCanMove = false;
-                        GAME_FILE_LOGGER.log("Ничья!");
-                    }
-                } catch (const std::exception &e) {
-                    ERROR_FILE_LOGGER.log(e.what());
-                }
-            }
+    while(window.isOpen()) {
+        _drawField(window); 
+        window.display();
+        while(window.pollEvent(event)) {
+            _handleEvent(event, window);
         }
+        window.clear(sf::Color::White); 
     }
+}
+
+void SFMLInterface::_handleEvent(sf::Event &event, sf::Window &window) {
+    switch(event.type) {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        case sf::Event::MouseButtonPressed:
+            if (event.mouseButton.button == sf::Mouse::Left && !_hasWinner) {
+                _handleUserStep(event);
+            }
+            if (!_hasWinner) {
+                _handleComputerStep();
+            }
+            break;
+    }
+}
+
+void SFMLInterface::_handleUserStep(sf::Event &event) {
+    if (!_tryToMakeUserStep(event)) return;
+    _checkForWinOrDraw("Игрок");
+}
+
+void SFMLInterface::_handleComputerStep() {
+    _core.computerStep();
+    _checkForWinOrDraw("Компьютер");
+}
+
+bool SFMLInterface::_tryToMakeUserStep(sf::Event &event) {
+    try {
+        _core.userStep(_getCoordinatesFromEvent(event)); 
+        return true; 
+    }
+    catch(const std::exception &e) {
+        ERROR_FILE_LOGGER.log(e.what());
+        return false; 
+    }
+}
+
+Coordinates SFMLInterface::_getCoordinatesFromEvent(sf::Event &event) {
+    int x = event.mouseButton.x / CELL_SIZE;
+    int y = event.mouseButton.y / CELL_SIZE;
+    return {x, y}; 
+}
+
+bool SFMLInterface::_checkForWinOrDraw(const std::string &player) {
+    if (_core.checkWinPosition()) {
+        _hasWinner = true;
+        GAME_FILE_LOGGER.log(player + " выиграл!");
+        return true; 
+    } 
+    else if (_core.checkDrawPosition()) {
+        _hasWinner = true;
+        return true;
+    }
+
+    return false;
 }
 
 void SFMLInterface::_drawField(sf::RenderWindow &window) {
@@ -68,48 +87,56 @@ void SFMLInterface::_drawField(sf::RenderWindow &window) {
 }
 
 void SFMLInterface::_drawCell(sf::RenderWindow &window, int x, int y) {
-    // Рисуем клетку
+    _drawEmptyCell(window, x, y);
+    CellState cellState = _field.getCellByCoordinates({x, y}).state;
+
+    if (cellState == CellState::CROSS) {
+        _drawCross(window, x, y); 
+    } 
+    else if (cellState == CellState::ZERO) {
+        _drawZero(window, x, y); 
+    }
+}
+
+void SFMLInterface::_drawEmptyCell(sf::RenderWindow &window, int x, int y) {
     sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
     cell.setPosition(x * CELL_SIZE, y * CELL_SIZE);
     cell.setFillColor(sf::Color::White);
     cell.setOutlineThickness(1);
     cell.setOutlineColor(sf::Color::Black);
     window.draw(cell);
+}
 
-    // Получаем состояние клетки
-    CellState cellState = _field.getCellByCoordinates({x, y}).state;
+void SFMLInterface::_drawCross(sf::RenderWindow &window, int x, int y) {
+    sf::RectangleShape line1(sf::Vector2f(CELL_SIZE * 0.7f, CELL_SIZE * 0.1f));
+    line1.setFillColor(sf::Color::Red);
+    line1.setOrigin(line1.getSize().x / 2, line1.getSize().y / 2);
+    line1.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
+    line1.setRotation(45); // Поворот первой линии
 
-    if (cellState == CellState::CROSS) {
-        // Рисуем крестик как два пересекающихся прямоугольника
-        sf::RectangleShape line1(sf::Vector2f(CELL_SIZE * 0.7f, CELL_SIZE * 0.1f));
-        line1.setFillColor(sf::Color::Red);
-        line1.setOrigin(line1.getSize().x / 2, line1.getSize().y / 2);
-        line1.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
-        line1.setRotation(45); // Поворот первой линии
+    sf::RectangleShape line2(sf::Vector2f(CELL_SIZE * 0.7f, CELL_SIZE * 0.1f));
+    line2.setFillColor(sf::Color::Red);
+    line2.setOrigin(line2.getSize().x / 2, line2.getSize().y / 2);
+    line2.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
+    line2.setRotation(-45); // Поворот второй линии
 
-        sf::RectangleShape line2(sf::Vector2f(CELL_SIZE * 0.7f, CELL_SIZE * 0.1f));
-        line2.setFillColor(sf::Color::Red);
-        line2.setOrigin(line2.getSize().x / 2, line2.getSize().y / 2);
-        line2.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
-        line2.setRotation(-45); // Поворот второй линии
+    window.draw(line1);
+    window.draw(line2);
+}
 
-        window.draw(line1);
-        window.draw(line2);
-    } else if (cellState == CellState::ZERO) {
-        // Рисуем нолик как круг с дыркой
-        sf::CircleShape outerCircle(CELL_SIZE / 2.5f);
-        outerCircle.setFillColor(sf::Color::White);
-        outerCircle.setOutlineThickness(5);
-        outerCircle.setOutlineColor(sf::Color::Blue);
-        outerCircle.setOrigin(outerCircle.getRadius(), outerCircle.getRadius());
-        outerCircle.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
+void SFMLInterface::_drawZero(sf::RenderWindow &window, int x, int y) {
+    sf::CircleShape outerCircle(CELL_SIZE / 2.5f);
+    outerCircle.setFillColor(sf::Color::White);
+    outerCircle.setOutlineThickness(5);
+    outerCircle.setOutlineColor(sf::Color::Blue);
+    outerCircle.setOrigin(outerCircle.getRadius(), outerCircle.getRadius());
+    outerCircle.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
 
-        sf::CircleShape innerCircle(CELL_SIZE / 4.0f);
-        innerCircle.setFillColor(sf::Color::White);
-        innerCircle.setOrigin(innerCircle.getRadius(), innerCircle.getRadius());
-        innerCircle.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
+    sf::CircleShape innerCircle(CELL_SIZE / 4.0f);
+    innerCircle.setFillColor(sf::Color::White);
+    innerCircle.setOrigin(innerCircle.getRadius(), innerCircle.getRadius());
+    innerCircle.setPosition(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2);
 
-        window.draw(outerCircle);
-        window.draw(innerCircle);
-    }
+    window.draw(outerCircle);
+    window.draw(innerCircle);
 }
