3a092d2d23f2a2fcabae20b3ebad1e3db090ecc4
artem <ArtemKorolev24092009@yandex.ru>
2025-03-30 20:33:29 +0300

    fix logger bug

--- Source/Logger.cpp
+++ Source/Logger.cpp
@@ -12,7 +12,7 @@ void FileLogger::log(const std::string &message) const {
 
 std::string FileLogger::_getLogStringByMessage(const std::string &message) const {
     std::string currentDate = utils::getCurrentDateTime();
-    std::string logString = "[" + currentDate + "] " + message + "\n";
+    return "[" + currentDate + "] " + message + "\n";
 }
 
 void ConsoleLogger::log(const std::string &message) const {
